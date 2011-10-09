4ea15a7a9f3d6c748675c49c87fae761bf30c11b
Caroline Leung <carolineleung@Caroline-Leungs-MacBook-Pro.local>
2011-10-09 19:08:34 -0400

    CL: Spacing changes (no code change)

--- ShoppingList/src/com/mcshoppinglist/app/checklist/CheckListActivity.java
+++ ShoppingList/src/com/mcshoppinglist/app/checklist/CheckListActivity.java
@@ -35,185 +35,180 @@ import com.mcshoppinglist.app.util.MCLogger;
  */
 public class CheckListActivity extends ListActivity {
 
-    public static final String[] PROJECTION = new String[] { ShoppingItems._ID,
-                    ShoppingItems.SHOPPING_LIST_ID, ShoppingItems.SHOPPING_ITEM_ID,
-                    ShoppingItems.ITEM, ShoppingItems.CHECKED, ShoppingItems.LABELS,
-                    ShoppingItems.ADDITIONAL_NOTES, ShoppingItems.POSITION,
-                    ShoppingItems.MODIFIED_DATE };
+	public static final String[] PROJECTION = new String[] { ShoppingItems._ID, ShoppingItems.SHOPPING_LIST_ID, ShoppingItems.SHOPPING_ITEM_ID, ShoppingItems.ITEM, ShoppingItems.CHECKED,
+			ShoppingItems.LABELS, ShoppingItems.ADDITIONAL_NOTES, ShoppingItems.POSITION, ShoppingItems.MODIFIED_DATE };
 
-    // cl: Managers resulted from refactoring
-    private ImportExportManager importExportMgr;
-    private RenderManager renderMgr;
-    private CheckListViewFinder viewFinder;
-    private String shoppingListId = AppConstants.INVALID_SHOPPING_LIST_ID;
+	// cl: Managers resulted from refactoring
+	private ImportExportManager importExportMgr;
+	private RenderManager renderMgr;
+	private CheckListViewFinder viewFinder;
+	private String shoppingListId = AppConstants.INVALID_SHOPPING_LIST_ID;
 
-    private ShoppingListItemDiffsReceiver diffsReceiver;
+	private ShoppingListItemDiffsReceiver diffsReceiver;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        initManagers();
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		initManagers();
 
-        setDefaultKeyMode(DEFAULT_KEYS_SHORTCUT);
-        setContentView(R.layout.checklist);
+		setDefaultKeyMode(DEFAULT_KEYS_SHORTCUT);
+		setContentView(R.layout.checklist);
 
-        // If no data was given in intent (as we started as a MAIN activity), use our default content provider.
-        Intent intent = getIntent();
-        shoppingListId = intent.getStringExtra(AppConstants.INTENT_KEY_SHOPPING_LIST_ID);
-        if (AppConstants.INVALID_SHOPPING_LIST_ID.equals(shoppingListId)) {
-            MCLogger.i(getClass(), "onCreate intent missing/invalid shoppingListId"); // TODO Remove this warning.
-            // TODO If there's no shopping list id, then we should create a new (empty) shopping list?
-        }
-        // TODO mel: We're not passing data with the intent (yet)... For now just use ShoppingItems.CONTENT_URI.
-        // if (intent.getData() == null) {
-        // intent.setData(ShoppingItems.CONTENT_URI);
+		// If no data was given in intent (as we started as a MAIN activity), use our default content provider.
+		Intent intent = getIntent();
+		shoppingListId = intent.getStringExtra(AppConstants.INTENT_KEY_SHOPPING_LIST_ID);
+		if (AppConstants.INVALID_SHOPPING_LIST_ID.equals(shoppingListId)) {
+			MCLogger.i(getClass(), "onCreate intent missing/invalid shoppingListId"); // TODO Remove this warning.
+			// TODO If there's no shopping list id, then we should create a new (empty) shopping list?
+		}
+		// TODO mel: We're not passing data with the intent (yet)... For now just use ShoppingItems.CONTENT_URI.
+		// if (intent.getData() == null) {
+		// intent.setData(ShoppingItems.CONTENT_URI);
 
-        addListItemsLabels();
-        initAutoComplete();
-        initListeners();
+		addListItemsLabels();
+		initAutoComplete();
+		initListeners();
 
-        renderMgr.refreshView(ViewMode.ViewAll, null);
+		renderMgr.refreshView(ViewMode.ViewAll, null);
 
-        refreshCheckList();
-    }
+		refreshCheckList();
+	}
 
-    private void initListeners() {
-        Button addButton = viewFinder.getAddItemButton();
-        addButton.setOnClickListener(new AddItemClickListener(viewFinder, this));
-    }
+	private void initListeners() {
+		Button addButton = viewFinder.getAddItemButton();
+		addButton.setOnClickListener(new AddItemClickListener(viewFinder, this));
+	}
 
-    private void initManagers() {
-        renderMgr = new RenderManager(this);
-        viewFinder = new CheckListViewFinder(this);
+	private void initManagers() {
+		renderMgr = new RenderManager(this);
+		viewFinder = new CheckListViewFinder(this);
 
-        // TODO cl: ImportExportManager needs to take in ShoppingList, because we are inserting directly into the DB as
-        // reading through files. This is not clean, but to preserve mem used. The alternative is to return some data
-        // structure that holds all the temp entries need to be inserted, and then insert them using contentMgr
-        importExportMgr = new ImportExportManager(this);
-    }
+		// TODO cl: ImportExportManager needs to take in ShoppingList, because we are inserting directly into the DB as
+		// reading through files. This is not clean, but to preserve mem used. The alternative is to return some data
+		// structure that holds all the temp entries need to be inserted, and then insert them using contentMgr
+		importExportMgr = new ImportExportManager(this);
+	}
 
-    private void addListItemsLabels() {
-        Cursor cur = managedQuery(ShoppingItems.CONTENT_URI, PROJECTION, null, null,
-                        ShoppingItems.DEFAULT_SORT_ORDER);
-        ContentManager contentMgr = getShoppingListApplication().getContentManager();
-        contentMgr.setNumItems(cur.getCount());
-        if (cur.isBeforeFirst()) {
-            cur.moveToFirst();
-        }
-        while (!cur.isAfterLast()) {
-            String label = cur.getString(cur.getColumnIndex(ShoppingItems.LABELS));
-            contentMgr.getLabelsSet().add(label);
-            if (!cur.moveToNext()) {
-                break;
-            }
-        }
-    }
+	private void addListItemsLabels() {
+		Cursor cur = managedQuery(ShoppingItems.CONTENT_URI, PROJECTION, null, null, ShoppingItems.DEFAULT_SORT_ORDER);
+		ContentManager contentMgr = getShoppingListApplication().getContentManager();
+		contentMgr.setNumItems(cur.getCount());
+		if (cur.isBeforeFirst()) {
+			cur.moveToFirst();
+		}
+		while (!cur.isAfterLast()) {
+			String label = cur.getString(cur.getColumnIndex(ShoppingItems.LABELS));
+			contentMgr.getLabelsSet().add(label);
+			if (!cur.moveToNext()) {
+				break;
+			}
+		}
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        renderMgr.onCreateOptionsMenu(menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		renderMgr.onCreateOptionsMenu(menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        renderMgr.onPrepareOptionsMenu(menu);
-        return super.onPrepareOptionsMenu(menu);
-    }
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		renderMgr.onPrepareOptionsMenu(menu);
+		return super.onPrepareOptionsMenu(menu);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        renderMgr.onOptionsItemSelected(item);
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		renderMgr.onOptionsItemSelected(item);
+		return super.onOptionsItemSelected(item);
+	}
 
-    private void initAutoComplete() {
-        Cursor cursor = managedQuery(ShoppingItems.CONTENT_URI, new String[] { ShoppingItems._ID,
-                        ShoppingItems.ITEM }, null, null, ShoppingItems.DEFAULT_SORT_ORDER);
-        ItemSuggestionCursorAdapter adapter = new ItemSuggestionCursorAdapter(this, cursor);
-        adapter.setFilterQueryProvider(new ItemNameFilterQueryProvider(this));
-        viewFinder.getInputText().setAdapter(adapter);
-    }
+	private void initAutoComplete() {
+		Cursor cursor = managedQuery(ShoppingItems.CONTENT_URI, new String[] { ShoppingItems._ID, ShoppingItems.ITEM }, null, null, ShoppingItems.DEFAULT_SORT_ORDER);
+		ItemSuggestionCursorAdapter adapter = new ItemSuggestionCursorAdapter(this, cursor);
+		adapter.setFilterQueryProvider(new ItemNameFilterQueryProvider(this));
+		viewFinder.getInputText().setAdapter(adapter);
+	}
 
-    public void updateListAdapter(Cursor cur) {
-        ShoppingListCursorAdapter listAdapter = new ShoppingListCursorAdapter(this, cur);
-        setListAdapter(listAdapter);
-        getListView().setOnItemLongClickListener(new ItemLongClickListener(this));
-    }
+	public void updateListAdapter(Cursor cur) {
+		ShoppingListCursorAdapter listAdapter = new ShoppingListCursorAdapter(this, cur);
+		setListAdapter(listAdapter);
+		getListView().setOnItemLongClickListener(new ItemLongClickListener(this));
+	}
 
-    private void refreshCheckList(boolean fullResync) {
-        Intent refreshCheckListIntent = new Intent(this, ShoppingListUpdateService.class);
-        refreshCheckListIntent.putExtra(AppConstants.INTENT_KEY_SHOPPING_LIST_ID, shoppingListId);
+	private void refreshCheckList(boolean fullResync) {
+		Intent refreshCheckListIntent = new Intent(this, ShoppingListUpdateService.class);
+		refreshCheckListIntent.putExtra(AppConstants.INTENT_KEY_SHOPPING_LIST_ID, shoppingListId);
 
-        if (fullResync) {
-            refreshCheckListIntent.putExtra(AppConstants.INTENT_REFRESH_FULL_LIST_KEY, true);
-        }
-        startService(refreshCheckListIntent);
-    }
+		if (fullResync) {
+			refreshCheckListIntent.putExtra(AppConstants.INTENT_REFRESH_FULL_LIST_KEY, true);
+		}
+		startService(refreshCheckListIntent);
+	}
 
-    public void refreshCheckList() {
-        refreshCheckList(true);
-    }
+	public void refreshCheckList() {
+		refreshCheckList(true);
+	}
 
-    @Override
-    protected void onResume() {
-        IntentFilter filter = new IntentFilter(ShoppingListUpdateService.INTENT_PROCESS_ITEM_DIFFS);
-        diffsReceiver = new ShoppingListItemDiffsReceiver();
-        registerReceiver(diffsReceiver, filter);
+	@Override
+	protected void onResume() {
+		IntentFilter filter = new IntentFilter(ShoppingListUpdateService.INTENT_PROCESS_ITEM_DIFFS);
+		diffsReceiver = new ShoppingListItemDiffsReceiver();
+		registerReceiver(diffsReceiver, filter);
 
-        // TODO refresh/load check list?
-        refreshCheckList(false);
-        super.onResume();
-    }
+		// TODO refresh/load check list?
+		refreshCheckList(false);
+		super.onResume();
+	}
 
-    @Override
-    protected void onPause() {
-        unregisterReceiver(diffsReceiver);
-        diffsReceiver = null;
+	@Override
+	protected void onPause() {
+		unregisterReceiver(diffsReceiver);
+		diffsReceiver = null;
 
-        Intent svc = new Intent(this, ShoppingListUpdateService.class);
-        stopService(svc);
+		Intent svc = new Intent(this, ShoppingListUpdateService.class);
+		stopService(svc);
 
-        super.onPause();
-    }
+		super.onPause();
+	}
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // Unregister listeners
-        getListView().setOnItemLongClickListener(null);
-        importExportMgr = null;
-        renderMgr = null;
-        viewFinder = null;
-    }
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// Unregister listeners
+		getListView().setOnItemLongClickListener(null);
+		importExportMgr = null;
+		renderMgr = null;
+		viewFinder = null;
+	}
 
-    public ImportExportManager getImportExportManager() {
-        return importExportMgr;
-    }
+	public ImportExportManager getImportExportManager() {
+		return importExportMgr;
+	}
 
-    public ShoppingListApplication getShoppingListApplication() {
-        return (ShoppingListApplication) getApplication();
-    }
+	public ShoppingListApplication getShoppingListApplication() {
+		return (ShoppingListApplication) getApplication();
+	}
 
-    public String getShoppingListId() {
-        return shoppingListId;
-    }
+	public String getShoppingListId() {
+		return shoppingListId;
+	}
 
-    private class ShoppingListItemDiffsReceiver extends BroadcastReceiver {
+	private class ShoppingListItemDiffsReceiver extends BroadcastReceiver {
 
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            ShoppingListApplication shoppingListApp = getShoppingListApplication();
-            List<ShoppingListItem> diffs = shoppingListApp.getMostRecentDiffs();
-            if (diffs != null && !diffs.isEmpty()) {
-                ContentManager contentMgr = getShoppingListApplication().getContentManager();
-                for (ShoppingListItem updatedItem : diffs) {
-                    contentMgr.updateItem(shoppingListId, updatedItem);
-                }
-            }
-            // Reset diffs to null
-            shoppingListApp.setMostRecentDiffs(null);
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			ShoppingListApplication shoppingListApp = getShoppingListApplication();
+			List<ShoppingListItem> diffs = shoppingListApp.getMostRecentDiffs();
+			if (diffs != null && !diffs.isEmpty()) {
+				ContentManager contentMgr = getShoppingListApplication().getContentManager();
+				for (ShoppingListItem updatedItem : diffs) {
+					contentMgr.updateItem(shoppingListId, updatedItem);
+				}
+			}
+			// Reset diffs to null
+			shoppingListApp.setMostRecentDiffs(null);
 
-        }
-    }
+		}
+	}
 }
